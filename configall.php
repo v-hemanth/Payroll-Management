304e6e2a56e8e1f8af10ce6cf4bf6b4c0e900c91
Hemanth <76104612+v-hemanth@users.noreply.github.com>
2021-04-13 10:41:24 +0530

    Create configall.php

---
+++ configall.php
@@ -0,0 +1,7 @@
+<?php
+define('db_server','localhost');
+define('db_username','root');
+define('db_password','');
+define('db_database','payrollmanagement');
+$connection=mysqli_connect(db_server,db_username,db_password,db_database);
+?>
