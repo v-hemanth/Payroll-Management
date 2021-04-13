b15b0c36f504bed0e67ba4766932c922e6784093
Hemanth <76104612+v-hemanth@users.noreply.github.com>
2021-04-13 10:52:14 +0530

    Create adddepts.js

---
+++ adddepts.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<title>Add department</title>
+<body>
+<div class="w3-container" style="padding: 50px 200px 20px 200px">
+<form class="w3-container" action="dep-addphp.php" method="post">
+<p><label>Department id</label>
+<input class="w3-input w3-light-grey w3-animate-input" type="number" name="depid"></p>
+<p>
+<label>Department Name</label>
+<input class="w3-input w3-light-grey w3-animate-input" type="text" name="depname">
+</p>
+<input type="submit" value="Submit" class="w3-input w3-green w3-round-xxlarge w3-animate-input">
+</form>
+</div></body></html>
